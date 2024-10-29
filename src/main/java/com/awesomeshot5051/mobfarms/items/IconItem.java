b5cb57076ec5b3e45e8d1a6138254380af13226f
awesomeshot5051 <83024106+awesomeshot5051@users.noreply.github.com>
2024-10-29 11:08:31 -0400

    Delete src/main directory

--- src/main/java/com/awesomeshot5051/mobfarms/items/IconItem.java
+++
@@ -1,9 +0,0 @@
-package com.awesomeshot5051.mobfarms.items;
-
-import net.minecraft.world.item.Item;
-
-public class IconItem extends Item {
-    public IconItem(Properties properties) {
-        super(properties);
-    }
-}
