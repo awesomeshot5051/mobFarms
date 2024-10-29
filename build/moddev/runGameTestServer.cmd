a2d8bd9da824d20b974553ddd2d9a7aff04d4eb7
awesomeshot5051 <83024106+awesomeshot5051@users.noreply.github.com>
2024-10-29 11:08:09 -0400

    Delete build directory

--- build/moddev/runGameTestServer.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-for /f "tokens=2 delims=:." %%x in ('chcp') do set _codepage=%%x
-chcp 65001>nul
-cd D:\MCreatorWorkspaces\mobFarms\MobFarms\run
-"C:\Program Files\Java\jdk-21\bin\java.exe" @D:\MCreatorWorkspaces\mobFarms\MobFarms\build\moddev\gameTestServerRunClasspath.txt @D:\MCreatorWorkspaces\mobFarms\MobFarms\build\moddev\gameTestServerRunVmArgs.txt -Dfml.modFolders=mob_farms%%%%D:\MCreatorWorkspaces\mobFarms\MobFarms\build\classes\java\main;mob_farms%%%%D:\MCreatorWorkspaces\mobFarms\MobFarms\build\resources\main net.neoforged.devlaunch.Main @D:\MCreatorWorkspaces\mobFarms\MobFarms\build\moddev\gameTestServerRunProgramArgs.txt
-if not ERRORLEVEL 0 (  echo Minecraft failed with exit code %ERRORLEVEL%  pause)
-chcp %_codepage%>nul
-endlocal
