1da01420ca0873852f5f437f99a4f0192910e553
awesomeshot5051 <traeengland01@Att.net>
2025-01-28 15:38:29 -0500

    1.21.1 V 2.0.2 Fixed enchantments not being kept when relogging. Fixed Picktype not being kept when relogging or minging.

--- src/main/java/com/awesomeshot5051/mobfarms/blocks/tileentity/aggressiveMobs/PillagerFarmTileentity.java
+++ src/main/java/com/awesomeshot5051/mobfarms/blocks/tileentity/aggressiveMobs/PillagerFarmTileentity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:44829c2db4597a85db65bd04ade99e36d8fa439b9d354c66be73ce63f466ae74
-size 8153
+oid sha256:2963c5e6e25d71f322954325dba35c5c48d42ee836499a5836efdc2f1fecf7f9
+size 8242
