1efe55f98433cecf8c1772dc27dcce3f904908a2
s412924 <traeengland01@Att.net>
2025-02-04 19:52:50 -0500

    1.21.1 V 2.0.3 Fixed drop bug on Rabbit Farm

--- src/main/java/com/awesomeshot5051/mobfarms/blocks/tileentity/passiveMobs/RabbitFarmTileentity.java
+++ src/main/java/com/awesomeshot5051/mobfarms/blocks/tileentity/passiveMobs/RabbitFarmTileentity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:130a89082be6a057b64c3542382524aef14b3b5b5399c09f5ad11d17f8de668c
-size 7495
+oid sha256:2dcbb5d7db329314165bb46ec5fc04f730d22adb65c93b7220687f6fa5d20d2f
+size 7553
